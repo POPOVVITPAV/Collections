27d0154eea8cd97b719673f69b71ff8d8057150e
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2021-07-08 06:28:30 +0300

    To be continio

--- src/ru/ain/Colection.java
+++ src/ru/ain/Colection.java
@@ -1,6 +1,18 @@
 package ru.ain;
 
-public class Colection {
+import java.util.ArrayDeque;
 
+public class Colection {
+	
+	public static void main(String[] args) {
+		ArrayDeque<String> states = new ArrayDeque<String>();
+		states.add("Germany");//стандартное добавление элементов
+		states.addFirst("France");//добавляем элемент в самое начало
+		states.push("ddddd");//добавляем элемент в самое начало
+		states.addLast("jhibhhvb");//добавляем элемент в конец коллекции
+		states.add("ojkherg");
+		
+		
+	}
 	
 }
