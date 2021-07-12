c4ee62e92dc3e0ebb3f35f1a6a7a5a2f87674287
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2021-07-12 05:54:06 +0300

    Collection + Deque

--- src/ru/ain/Colection.java
+++ src/ru/ain/Colection.java
@@ -12,7 +12,35 @@ public class Colection {
 		states.addLast("jhibhhvb");//добавляем элемент в конец коллекции
 		states.add("ojkherg");
 		
+		String sFirst = states.getFirst();
+		System.out.println(sFirst);
 		
+		String sLast = states.getLast();
+		System.out.println(sLast);
+		
+		System.out.printf("Quee size: %d \n", states.size());
+		
+		while(states.peek()!=null) {
+			System.out.print(states.pop()+" ");
+		}
+		System.out.println();
+		ArrayDeque<Person> people = new ArrayDeque<Person>();
+		people.addFirst(new Person("Tom"));
+		people.addLast(new Person("Ann"));
+		for(Person p:people) {
+			System.out.print(p.getName()+" ");
+		}
 	}
+
 	
+}
+
+class Person{
+	private String name;
+	public Person(String value) {
+		this.name=value;
+	}
+	String getName() {
+		return name;
+	}
 }
